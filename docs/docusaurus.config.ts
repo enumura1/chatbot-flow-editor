3fe5abd3c5dc2cfce3e157e84e249175cee2ba07
enumura1 <akirayuto0718@gmail.com>
2025-06-05 02:16:55 +0900

    docs:add page img on herosection

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -179,7 +179,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} Chatbot Flow Editor. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} enumura1. Built with Docusaurus.`,
     },
     
     prism: {
